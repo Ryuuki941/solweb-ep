d72cdd6f613eab8ca33fc8e0f8abca7680feab27
Enzo Yamada Satake <enzoyamada@hotmail.com>
2017-12-13 12:07:01 -0200

    funciona barra de busca

--- js/func.js
+++ js/func.js
@@ -1,18 +1,20 @@
 
+// var busCheck = document.getElementById("check-bus").checked;
+// var stopCheck = document.getElementById("check-stops").checked
+var geocoder
+
 function initMap() {
-
-var busCheck = document.getElementById("check-bus").checked;
-var stopCheck = document.getElementById("check-stops").checked
-
-  var uluru = {lat: -23.4829, lng:  -46.501};
+  geocoder = new google.maps.Geocoder();
+  var latlng = {lat: -23.4829, lng:  -46.501};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 20,
-    center: uluru,
+    zoom: 18,
+    center: latlng,
     mapTypeId: google.maps.MapTypeId.SATELLITE,
-    zoom: 18 
   });
-
-    var infoWindow = new google.maps.InfoWindow({map: map});
+  var marker = new google.maps.Marker({
+    position: latlng,
+    map: map
+  });
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
@@ -22,81 +24,158 @@ var stopCheck = document.getElementById("check-stops").checked
         lng: position.coords.longitude
       };
 
-      infoWindow.setPosition(pos);
-      infoWindow.setContent('Location found.');
+      marker.setPosition(pos);
       map.setCenter(pos);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
-    var marker = new google.maps.Marker({
-      position: pos,
-      map: map
-    });
+    
+    
   } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
   }
 
 
-  if(stopCheck == true){
+//teste tudo + autocomplete
+      
+      function carregarNoMapa(endereco) {
+        geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+              var latitude = results[0].geometry.location.lat();
+              var longitude = results[0].geometry.location.lng();
+        
+              $('#txtEndereco').val(results[0].formatted_address);
+              $('#txtLatitude').val(latitude);
+              $('#txtLongitude').val(longitude);
+        
+              var location = new google.maps.LatLng(latitude, longitude);
+              marker.setPosition(location);
+              map.setCenter(location);
+              map.setZoom(16);
+            }
+          }
+        })
+      }
+      
+      $("#btnEndereco").click(function() {
+        if($(this).val() != "")
+          carregarNoMapa($("#txtEndereco").val());
+      })
+      
+      $("#txtEndereco").blur(function() {
+        if($(this).val() != "")
+          carregarNoMapa($(this).val());
+      })
+      
+      google.maps.event.addListener(marker, 'drag', function () {
+        geocoder.geocode({ 'latLng': marker.getPosition() }, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {  
+              $('#txtEndereco').val(results[0].formatted_address);
+              $('#txtLatitude').val(marker.getPosition().lat());
+              $('#txtLongitude').val(marker.getPosition().lng());
+            }
+          }
+        });
+      });
+      
+      $("#txtEndereco").autocomplete({
+        source: function (request, response) {
+          geocoder.geocode({ 'address': request.term + ', Brasil', 'region': 'BR' }, function (results, status) {
+            response($.map(results, function (item) {
+              return {
+                label: item.formatted_address,
+                value: item.formatted_address,
+                latitude: item.geometry.location.lat(),
+                      longitude: item.geometry.location.lng()
+              }
+            }));
+          })
+        },
+        select: function (event, ui) {
+          $("#txtLatitude").val(ui.item.latitude);
+            $("#txtLongitude").val(ui.item.longitude);
+          var location = new google.maps.LatLng(ui.item.latitude, ui.item.longitude);
+          marker.setPosition(location);
+          map.setCenter(location);
+          map.setZoom(16);
+        }
+      });
+      
+      $("form").submit(function(event) {
+        event.preventDefault();
+        
+        var endereco = $("#txtEndereco").val();
+        var latitude = $("#txtLatitude").val();
+        var longitude = $("#txtLongitude").val();
+        
+        alert("Endereço: " + endereco + "\nLatitude: " + latitude + "\nLongitude: " + longitude);
+      });
+
+
+  // if(stopCheck == true){
+
+  //   console.log("oi")
 
 
    
-    var lat_min = pos.lat - 0.0045;
-    var lat_max = pos.lat + 0.0045;
-    var lon_min = pos.lon - 0.0045;
-    var lon_max = pos.lon + 0.0045;
+  //   var lat_min = pos.lat - 0.0045;
+  //   var lat_max = pos.lat + 0.0045;
+  //   var lon_min = pos.lon - 0.0045;
+  //   var lon_max = pos.lon + 0.0045;
 
-    var local = {
-          "lat_min": lat_min,
-          "lat_max": lat_max,
-          "lon_min": lon_min,
-          "lon_max": lon_max
-    }
+  //   var local = {
+  //         "lat_min": lat_min,
+  //         "lat_max": lat_max,
+  //         "lon_min": lon_min,
+  //         "lon_max": lon_max
+  //   }
 
-    var sender = JSON.stringify(local);
+  //   var sender = JSON.stringify(local);
 
 
-    request= new XMLHttpRequest()
-    var uri = encodeURIComponent(sender) 
-    request.open("GET", "/php/stopRequest.php?data="+uri, true)
-    request.setRequestHeader("Content-type", "application/json")
-    request.send()
+  //   request= new XMLHttpRequest()
+  //   var uri = encodeURIComponent(sender) 
+  //   request.open("GET", "/php/stopRequest.php?data="+uri, true)
+  //   request.setRequestHeader("Content-type", "application/json")
+  //   request.send()
     
 
 
-    window.location.href = "stopRequest.php?lat_min=" + lat_min;
-    window.location.href = "stopRequest.php?lat_max=" + lat_max;
-    window.location.href = "stopRequest.php?lon_min=" + lon_min;
-    window.location.href = "stopRequest.php?lon_max=" + lon_max;
+  //   window.location.href = "stopRequest.php?lat_min=" + lat_min;
+  //   window.location.href = "stopRequest.php?lat_max=" + lat_max;
+  //   window.location.href = "stopRequest.php?lon_min=" + lon_min;
+  //   window.location.href = "stopRequest.php?lon_max=" + lon_max;
     
 
 
-    //fazer busca no bd das posicoes de pontos dentro de 1km
-    //SELECT FROM stops WHERE( stop_lat BETWEEN value_1 AND value_2) AND ( stop_lon BETWEEN value_1 AND value_2) GROUP BY stop_id;
-  }
+  //   //fazer busca no bd das posicoes de pontos dentro de 1km
+  //   //SELECT FROM stops WHERE( stop_lat BETWEEN value_1 AND value_2) AND ( stop_lon BETWEEN value_1 AND value_2) GROUP BY stop_id;
+  // }
 
 
 
-  function showUser(str) {
-    if (str=="") {
-      document.getElementById("txtHint").innerHTML="";
-      return;
-    } 
-    if (window.XMLHttpRequest) {
-      // code for IE7+, Firefox, Chrome, Opera, Safari
-      xmlhttp=new XMLHttpRequest();
-    } else { // code for IE6, IE5
-      xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function() {
-      if (this.readyState==4 && this.status==200) {
-        document.getElementById("txtHint").innerHTML=this.responseText;
-      }
-    }
-    xmlhttp.open("GET","getuser.php?q="+str,true);
-    xmlhttp.send();
-  }
+  // function showUser(str) {
+  //   if (str=="") {
+  //     document.getElementById("txtHint").innerHTML="";
+  //     return;
+  //   } 
+  //   if (window.XMLHttpRequest) {
+  //     // code for IE7+, Firefox, Chrome, Opera, Safari
+  //     xmlhttp=new XMLHttpRequest();
+  //   } else { // code for IE6, IE5
+  //     xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  //   }
+  //   xmlhttp.onreadystatechange=function() {
+  //     if (this.readyState==4 && this.status==200) {
+  //       document.getElementById("txtHint").innerHTML=this.responseText;
+  //     }
+  //   }
+  //   xmlhttp.open("GET","getuser.php?q="+str,true);
+  //   xmlhttp.send();
+  // }
 
 
 
@@ -110,9 +189,10 @@ var stopCheck = document.getElementById("check-stops").checked
 }
   
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                        'Error: The Geolocation service failed.' :
-                        'Error: Your browser doesn\'t support geolocation.');
-}
+// function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//   infoWindow.setPosition(pos);
+//   infoWindow.setContent(browserHasGeolocation ?
+//                         'Error: The Geolocation service failed.' :
+//                         'Error: Your browser doesn\'t support geolocation.');
+// }
+
--- js/geo.js
+++ js/geo.js
@@ -11,15 +11,43 @@ geocoder = new google.maps.Geocoder();
 
 function getLocation( address, callback ){
     geocoder.geocode({address = address}, function(results, status){
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
             coordinates_obj = results[0].geometry.location;
+ 
             say(coordinates_obj.nb)
-            say(coordinates_obj.ob)
-            callback(coordinates)
+             say(coordinates_obj.ob)
+            callback(coordinates_obj)
+            var location = new google.maps.LatLng(latitude, longitude);
+            marker.setPosition(location);
+            map.setCenter(location);
+            map.setZoom(1);
+            }}
     })
-    return coordinates;
+    return coordinates_obj;
 }
 
 
 function say(something){
     console.log(something);
+}
+
+function carregarNoMapa(endereco) {
+    geocoder.geocode({ 'address': endereco + ', Brasil', 'region': 'BR' }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
+                var latitude = results[0].geometry.location.lat();
+                var longitude = results[0].geometry.location.lng();
+    
+                $('#txtEndereco').val(results[0].formatted_address);
+                $('#txtLatitude').val(latitude);
+                   $('#txtLongitude').val(longitude);
+    
+                var location = new google.maps.LatLng(latitude, longitude);
+                marker.setPosition(location);
+                map.setCenter(location);
+                map.setZoom(16);
+            }
+        }
+    })
 }
